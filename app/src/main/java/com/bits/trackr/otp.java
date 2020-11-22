124cf3dac7503e49bde5a879cb7a8f2f5b8992fa
Anjel-Patel <anjelpatel54@gmail.com>
2020-11-23 03:20:04 +0530

    Changes to layout, added Incorrect OTP message

--- app/src/main/java/com/bits/trackr/otp.java
+++ app/src/main/java/com/bits/trackr/otp.java
@@ -1,17 +1,35 @@
 package com.bits.trackr;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import org.w3c.dom.Text;
 
 public class otp extends Activity {
+    View field1;
+    View field2;
+    View field3;
+    View field4;
+    ConstraintLayout Layout_curr;
+    TextView error_message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+        Layout_curr = (ConstraintLayout)findViewById(R.id.otp_layout);
+        field1 = (View)findViewById(R.id.otp_phone_1);
+        field2 = (View)findViewById(R.id.otp_phone_2);
+        field3 = (View)findViewById(R.id.otp_phone_3);
+        field4 = (View)findViewById(R.id.otp_phone_4);
+        error_message = (TextView) findViewById(R.id.error_message_textview);
+        Layout_curr.removeView(error_message);
     }
 
     @Override
@@ -21,11 +39,20 @@ public class otp extends Activity {
         findViewById(R.id.verify_otp_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(true) //Condition to verify OTP
+                if(false) //Condition to verify OTP
                 {
                     Intent otp_to_emailverification = new Intent(otp.this, emailVerification.class);
                     startActivity(otp_to_emailverification);
                     finish();
+                }
+                else
+                {
+                    field1.setBackground(getDrawable(R.drawable.input_wrong));
+                    field2.setBackground(getDrawable(R.drawable.input_wrong));
+                    field3.setBackground(getDrawable(R.drawable.input_wrong));
+                    field4.setBackground(getDrawable(R.drawable.input_wrong));
+
+                    Layout_curr.addView(error_message);
                 }
             }
         });
@@ -39,3 +66,8 @@ public class otp extends Activity {
         finish();
     }
 }
+
+//public class TextWatch
+//{
+//    public
+//}
