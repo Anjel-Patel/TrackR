2dd478e7a9fd76fd9ee120734f7e0990d98e17ae
Graphici <graphici.official@gmail.com>
2020-11-23 21:32:43 +0530

    dashboard screen test v1.0

--- app/src/main/java/com/bits/trackr/dashboard.java
+++ app/src/main/java/com/bits/trackr/dashboard.java
@@ -1,14 +1,47 @@
 package com.bits.trackr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.bits.trackr.Adapter.ToDoAdapter;
+import com.bits.trackr.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class dashboard extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private List<ToDoModel> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.tasksText);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a test Task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
     }
 }
