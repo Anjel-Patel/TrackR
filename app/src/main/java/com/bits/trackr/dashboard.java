0878f0c05185f870a18976841ac07d1a5e06b244
Anjel-Patel <anjelpatel54@gmail.com>
2020-11-27 19:21:39 +0530

    Fixed fragment transactions

--- app/src/main/java/com/bits/trackr/dashboard.java
+++ app/src/main/java/com/bits/trackr/dashboard.java
@@ -2,6 +2,7 @@ package com.bits.trackr;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.ItemTouchHelper;
@@ -36,8 +37,6 @@ public class dashboard extends AppCompatActivity {
     //private TaskAdapter tasksAdapter;
     // private List<TaskModel> toDoModelList;
     FirebaseAuth fAuth;
-    FragmentManager fragment_manager;
-    FragmentTransaction fragment_transaction;
     TabLayout switcher_tab;
     FirebaseUser user;
     FloatingActionButton addTask;
@@ -45,6 +44,9 @@ public class dashboard extends AppCompatActivity {
     private CollectionReference taskRef=db.collection("tasks");
     private TaskAdapter adapter;
     Button profile_button;
+
+    Fragment todo_fragment;
+    Fragment cat_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +56,52 @@ public class dashboard extends AppCompatActivity {
         addTask=findViewById(R.id.addNewTask);
         profile_button = (Button)findViewById(R.id.profile_button);
 
-//        switcher_tab = findViewById(R.id.tabLayout);
-//        fragment_manager = getSupportFragmentManager();
-//        fragment_transaction = fragment_manager.beginTransaction();
-//        fragment_transaction.setReorderingAllowed(true)
-//                .add(R.id.dashboard_fragment_container, Todo_Fragment.class, null)
-//                .commit();
+        todo_fragment = new Todo_Fragment();
+        cat_fragment = new Cat_Fragment();
+        switcher_tab = findViewById(R.id.tabLayout);
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.dashboard_fragment_container, todo_fragment, null)
+                .commit();
+
+        switcher_tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition())
+                {
+                    case 0:
+                    {
+//                        Toast.makeText(getBaseContext(), "TODO clicked", Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction()
+                                .setReorderingAllowed(true)
+                                .replace(R.id.dashboard_fragment_container, todo_fragment, null)
+                                .commit();
+                        break;
+                    }
+                    case 1:
+                    {
+//                        Toast.makeText(getBaseContext(), "Categories clicked", Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction()
+                                .setReorderingAllowed(true)
+                                .replace(R.id.dashboard_fragment_container, cat_fragment, null)
+                                .commit();
+                        break;
+                    }
+                    default:
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
         addTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
