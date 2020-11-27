9eb0b58f0882d0d6fb4dfe390f07341b7a29178b
Anjel-Patel <anjelpatel54@gmail.com>
2020-11-27 07:17:38 +0530

    Added fragment classes and a few more cat card drawables

---
+++ app/src/main/java/com/bits/trackr/Todo_Fragment.java
@@ -0,0 +1,22 @@
+package com.bits.trackr;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Todo_Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_todo, container, false);
+    }
+}
